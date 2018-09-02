15014928a538648081a927a29180830e3c267296
kate.koppel <katie.spb@gmail.com>
2018-09-02 15:28:30 +0300

    router

--- server.js
+++ server.js
@@ -3,89 +3,22 @@ var bodyParser = require('body-parser');
 const path = require('path')
 
 const SERVER_PORT = 8080;
-
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/spacebookDB', function () {
   console.log("DB connection established!!!");
 })
-var { Post, Comment } = require('./models/postModel');
 
 var app = express();
+const api = require('./server/routes/api')
 app.use(express.static('public'))
 app.use(express.static('node_modules'))
 app.use(express.static('models'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
-
-app.get('/posts', (req, res) => {
-  Post.find().exec(function (err, posts) {
-    res.send(posts)
-  })
-})
-
-// adds new post to DB and sends back added post with id from DB
-app.post('/add-new-post', (req, res) => {
-  let newPost = new Post(req.body)
-  newPost.save()
-  res.send(newPost)
-})
-
-// removes post from DB
-app.delete('/remove-post/:id', (req, res) => {
-  let id = req.params.id
-  Post.findByIdAndRemove(id).exec(function (err, res) { })
-})
-
-// adds comment to DB
-app.post('/posts/:postId/add-comment', (req, res) => {
-  let id = req.params.postId
-  Post.findByIdAndUpdate(id, { $push: { comments: req.body } },
-    { new: true }, (err, post) => {
-      if (err) throw err;
-      else {
-        let len = post.comments.length
-        res.send(post.comments[len - 1])
-      }
-    })
-})
-
-// removes comment from DB
-app.delete('/posts/:postId/remove-comment/:commentId', (req, res) => {
-  let postId = req.params.postId
-  let commentId = req.params.commentId
-
-  Post.findById(postId).exec(function (err, currPost) {
-    currPost.comments.id(commentId).remove();
-    Post.findByIdAndUpdate(postId, currPost).exec(function (err, updatedPost) {
-    })
-  })
-})
-
-
+app.use('/', api)
 app.listen(SERVER_PORT, () => {
   console.log("Server started on port " + SERVER_PORT);
 })
 
 
 
-/*Post.findById(id).exec(function (err, currPost) {
-    currPost.comments.push(new Comment(req.body))
-
-    Post.findByIdAndUpdate(id, currPost).exec(function (err, updatedPost) {
-      let len = currPost.comments.length
-      res.send(currPost.comments[len - 1]) // sending added comment back
-    })
-})*/
-
-/*
-Post.findByIdAndUpdate(req.body.postId, 
-    {$pull: 
-      {comments: {
-        _id: req.body.commentId}
-      }
-    }, {new: true}, (err, post) => {
-      if (err) throw err;
-      else res.send(post)
-    })
-  })
-*/
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,94 +1,75 @@
-/*const express = require('express')
-var bodyParser = require('body-parser')
-//const path = require('path')
-var mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/spacebookDB', function () {
-  console.log("DB connection established!!!");
-})
+var express = require('express');
+var bodyParser = require('body-parser');
+const path = require('path')
 
-var Post = require('./models/postModel.js')
-var Comment = require('./models/postModel.js')
+var { Post, Comment } = require('../../models/postModel');
 
-//const request = require('request')
 const router = express.Router()
 
-//returns all the posts (and their comments)
 router.get('/posts', (req, res) => {
-  Post.findOne().exec(function(err, res){
-    console.log(res)
-    res.json(res)
-  }); 
-})
-*/
-// You will need to create 5 server routes
-// These will define your API:
-
-// 1) to handle getting all posts and their comments
-// 2) to handle adding a post
-// 3) to handle deleting a post
-// 4) to handle adding a comment to a post
-// 5) to handle deleting a comment from a post
-/*
-router.get('/hosts', (req, res) => {
-  fs.readFile(HOSTS_FILE, 'utf8', function (err, data) {
-    if (err) throw err
-    res.json(data)
-  });
-})
-
-router.post('/write-to-hosts', (req, res) => {
-  var newHost = { name: req.body.name, text: req.body.text }
-  var hosts = []
-
-  fs.readFile(HOSTS_FILE, 'utf8', function (err, data) {
-    if (err) throw err
-
-    if (data !== "") {
-      hosts = JSON.parse(data)
-    }
-
-    hosts.push(newHost)
-
-    fs.writeFile(HOSTS_FILE, JSON.stringify(hosts), function (err) {
-      if (err) throw err
-      else console.log('Data saved to hosts file!')
-    });
-
-  });
-})
-
-// Receive secret of someone else
-router.post('/secret', (req, res) => {
-  var newSecret = { name: req.body.name, secret: req.body.secret }
-  classmatesSecrets.push(newSecret)
-  response = { secretResponse: "Thank you for sharing your secret with me." }
-  res.json(response)
-})
-
-// Send secret to someone else
-router.post('/send-secret', (req, res) => {
-  let server = req.body.server
-  let url = "https://" + server + ".serveo.net/secret"
-  let data = {
-      name: 'kate',
-      secret: 'i like the beach'
-    }
-
-  const options = {
-      method: 'POST',
-      uri: url,
-      body: data,
-      json: true 
-        // JSON stringifies the body automatically
-  }
-
-  request.post(options, function (error, response, body) {
-    console.log('error:', error); // Print the error if one occurred
-    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    console.log('body:', body); // Print the HTML for the Google homepage.
-    res.json(body)
+  Post.find().exec(function (err, posts) {
+    res.send(posts)
   })
 })
-*/
-//module.exports = router
 
+// adds new post to DB and sends back added post with id from DB
+router.post('/add-new-post', (req, res) => {
+  let newPost = new Post(req.body)
+  newPost.save()
+  res.send(newPost)
+})
+
+// removes post from DB
+router.delete('/remove-post/:id', (req, res) => {
+  let id = req.params.id
+  Post.findByIdAndRemove(id).exec(function (err, res) { })
+})
+
+// adds comment to DB
+router.post('/posts/:postId/add-comment', (req, res) => {
+  let id = req.params.postId
+  Post.findByIdAndUpdate(id, { $push: { comments: req.body } },
+    { new: true }, (err, post) => {
+      if (err) throw err;
+      else {
+        let len = post.comments.length
+        res.send(post.comments[len - 1])
+      }
+    })
+})
+
+// removes comment from DB
+router.delete('/posts/:postId/remove-comment/:commentId', (req, res) => {
+  let postId = req.params.postId
+  let commentId = req.params.commentId
+
+  Post.findById(postId).exec(function (err, currPost) {
+    currPost.comments.id(commentId).remove();
+    Post.findByIdAndUpdate(postId, currPost).exec(function (err, updatedPost) {
+    })
+  })
+})
+
+module.exports = router
+
+/*Post.findById(id).exec(function (err, currPost) {
+    currPost.comments.push(new Comment(req.body))
+
+    Post.findByIdAndUpdate(id, currPost).exec(function (err, updatedPost) {
+      let len = currPost.comments.length
+      res.send(currPost.comments[len - 1]) // sending added comment back
+    })
+})*/
+
+/*
+Post.findByIdAndUpdate(req.body.postId, 
+    {$pull: 
+      {comments: {
+        _id: req.body.commentId}
+      }
+    }, {new: true}, (err, post) => {
+      if (err) throw err;
+      else res.send(post)
+    })
+  })
+*/
